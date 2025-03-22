7dcf1fea60aaae45090f7500e0ec7df9c20f2377
Mugi <129372596+Mugi94@users.noreply.github.com>
2025-03-22 16:23:35 +0100

    Add dependency injection to builder services

--- AlbumManager.Hosting/Program.cs
+++ AlbumManager.Hosting/Program.cs
@@ -1,3 +1,9 @@
+using BusinessObjects.Entity;
+using DataAccessLayer.Contexts;
+using DataAccessLayer.Repository;
+using Microsoft.EntityFrameworkCore;
+using Services.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +12,19 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Add custom services
+builder.Services.AddScoped<IGenericRepository<Artist>, ArtistRepository>();
+builder.Services.AddScoped<IGenericRepository<Record>, RecordRepository>();
+builder.Services.AddScoped<IGenericRepository<Track>, TrackRepository>();
+
+builder.Services.AddScoped<IArtistManager, ArtistManager>();
+builder.Services.AddScoped<IRecordManager, RecordManager>();
+builder.Services.AddScoped<ITrackManager, TrackManager>();
+
+builder.Services.AddDbContext<MusicContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"))
+);
 
 var app = builder.Build();
 
