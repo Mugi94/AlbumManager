b53d2e1eca17fcf8c8201e123a1d9e4735bcf34e
Mugi <129372596+Mugi94@users.noreply.github.com>
2025-10-16 22:49:02 +0200

    Add TypeRecord enum convertion

--- DataAccessLayer/Contexts/MusicContext.cs
+++ DataAccessLayer/Contexts/MusicContext.cs
@@ -1,4 +1,5 @@
 using BusinessObjects.Entity;
+using BusinessObjects.Enum;
 using Microsoft.EntityFrameworkCore;
 
 namespace DataAccessLayer.Contexts
@@ -8,9 +9,24 @@ namespace DataAccessLayer.Contexts
         public DbSet<Artist> Artists { get; set; }
         public DbSet<Record> Records { get; set; }
         public DbSet<Track> Tracks { get; set; }
-        
+
         public MusicContext(DbContextOptions<MusicContext> options) : base(options)
         {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Record>()
+                .Property(e => e.Type)
+                .HasConversion(
+                    v => v.ToString(),
+                    v => (TypeRecord)Enum.Parse(typeof(TypeRecord), v)
+                );
+        }
+        
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
         }
     }
 }
---
+++ DataAccessLayer/Migrations/20251016204606_ConvertTypeRecord.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAccessLayer.Migrations
+{
+    /// <inheritdoc />
+    public partial class ConvertTypeRecord : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Type",
+                table: "Records",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "integer");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Type",
+                table: "Records",
+                type: "integer",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+        }
+    }
+}
